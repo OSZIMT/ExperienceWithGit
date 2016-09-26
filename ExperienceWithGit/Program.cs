ad44ad923ea9991599231f4bff847ab95b264827
Urs <mutti@istdie.beste>
2016-09-26 10:51:56 +0200

    Momentanes Datum wird ausgegeben

--- ExperienceWithGit/Program.cs
+++ ExperienceWithGit/Program.cs
@@ -6,8 +6,8 @@ namespace ExperienceWithGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Today: 12.12.1995");
-			Console.WriteLine("Developer in this Project are: ");
+            Console.WriteLine(DateTime.Now);
+	    Console.WriteLine("Developer in this Project are: ");
             
             //TODO: Write your name here and push all your changes to Github.
             Console.WriteLine("John Doe,");
