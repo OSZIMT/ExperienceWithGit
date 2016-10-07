57f2bc0c9f463d6e71672144f333a1a8bd46dab8
mahnouel <mylotens@googlemail.com>
2016-10-07 10:53:29 +0200

    added Manuel

--- ExperienceWithGit/Program.cs
+++ ExperienceWithGit/Program.cs
@@ -7,21 +7,18 @@ namespace ExperienceWithGit
         static void Main(string[] args)
         {
             Console.WriteLine(DateTime.Now);
-	    Console.WriteLine("Developer in this Project are: ");
-<<<<<<< HEAD
+	        Console.WriteLine("Developer in this Project are: ");
             
             //TODO: Write your name here and push all your changes to Github.
             Console.WriteLine("John Doe,");
             Console.WriteLine("Jane Doe,");
             Console.WriteLine("Matthias Schernekau");
-            Console.WriteLine(",...");
-	    //Dies ist ein nutzloser Comment
+            Console.WriteLine("Manuel Klose");
+	        //Dies ist ein nutzloser Comment
             Console.ReadLine();
 
 		Console.WriteLine("Dorus ist die coolste Person in this Universe");
-=======
 		Console.WriteLine("Wer nicht da ist hat Durchfall");
->>>>>>> 0beb06c2d778c6a9cce1cdf8445266727b42eaea
         }
     }
 }
